9c5ae623c8efb2c70441ed4f90bfee9da56df608
brisajazminc <osandoval710@gmail.com>
2016-11-18 12:06:44 -0700

    ejemplo php

---
+++ ejemplo.php
@@ -0,0 +1,15 @@
+
+<form action="">
+	<input type="text" name="txtUsuario" id="txtUsuario">
+	<input type="text" name="txtNombre" id="txtNombre">
+	<input type="password" name="txtClave" id="txtClave">
+	<select name="txtTipo" id="txtTipo">
+		<option value="administrador">Administrador</option>
+		<option value="invitado">Invitado</option>
+		<option value="colado">Colado</option>
+	</select>
+	<input type="submit" value="Enviar">
+</form>
+
+<?php
+?>
