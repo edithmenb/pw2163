344906089d1dbb38dbe03c6acc764a7496b96034
edithmenb <edith.mendoza.barraza@gmail.com>
2016-12-02 02:53:59 -0700

    usuarios casi listo

--- ejemplo.php
+++ ejemplo.php
@@ -13,23 +13,23 @@
 </form>
 <hr>
 <?php
-	//Conexion al servidor
-	$conexion = mysql_connect("localhost","root","")
+	//Conecto al servidor
+	$conexion = mysql_connect("localhost","root","");
 	mysql_select_db("bd2164");
-	$consulta = "select * from usuarios ortder by usuario";
-	$resultado = mysql_query("consulta");//Ejecutando consulta
+	$consulta  = "select * from usuarios order by usuario";
+	$resultado = mysql_query($consulta);//Ejecutando consulta
 	$tabla = "<table border=1>";
-	$tabla .= "<tr>";
-	$tabla .= "<th>Usuario</th><th>Nombre</th><th>Clave</th><th>Tipo</th>";
-	$tabla .= "<tr>";
-
-	while($registro = mysql_fetch_array($resultado))//mientras sea posible hacer esa asignacion (sea)
+	$tabla.="<tr>";
+	$tabla.="<th>Usuario</th><th>Nombre</th><th>Clave</th><th>Tipo</th>";
+	$tabla.="</tr>";
+	while($registro = mysql_fetch_array($resultado))
 	{
-		$tabla .= "<tr>";
-		$tabla .= "<td>".registro["usuario"]."</td>";
-		$tabla .= "<td>".registro["nombre"]."</td>";
-		$tabla .= "<td>".registro["clave"]."</td>";
-		$tabla .= "<td>".registro["tipousuario"]."</td>";
+		$tabla.="<tr>";
+		$tabla.="<td>".$registro["usuario"]."</td>";
+		$tabla.="<td>".$registro["nombre"]."</td>";
+		$tabla.="<td>".$registro["clave"]."</td>";
+		$tabla.="<td>".$registro["tipo"]."</td>";	
+		$tabla.="</tr>";
 	}
 	$tabla.="</table>";
 	print $tabla;
--- usuarios/js/eventos.js
+++ usuarios/js/eventos.js
@@ -13,7 +13,6 @@ var inicioUsuarios = function()
 						"&clave="+clave+
 						"&id="+Math.random();
 		//Hacemos la peticion remota
-
 		//Validamos que no estén vacíos
 		if(usuario!="" && clave!="")
 		{
@@ -34,7 +33,6 @@ var inicioUsuarios = function()
 					{
 						alert("Datos incorrectos :(");
 					}
-					
 				},
 				error:function(xhr, ajaxOptions, thrownError){
 					//Si todo sale mal
@@ -45,10 +43,8 @@ var inicioUsuarios = function()
 		{
 			alert("Usuario y clave son obligatorios");
 		}
-
-
 	}
-	$("#btnValidaUsuario").on("click",validaUsuario);
+	
 	var teclaClave = function(tecla)
 	{
 		if (tecla.which == 13) //Tecla enter
@@ -56,20 +52,212 @@ var inicioUsuarios = function()
 			validaUsuario(); //funcion que valida al usuario
 		}
 	}
+
+	var GuardaUsuario = function()
+	{
+		//javascript se conecta a php y este devuelve un JSON
+		//Extraer los datos de los inputs en el HTML
+		var usuario= $("#txtUsuarioAlta").val();
+		var nombre= $("#txtNombreAlta").val();
+		var clave= $("#txtClaveAlta").val();
+		var tipo= $("#txtTipoAlta").val();
+
+		//Preparar los parámetros para AJAX
+		var parametros="opcion=guarda"+
+						"&usuario="+usuario+
+						"&nombre="+nombre+
+						"&clave="+clave+
+						"&tipo="+tipo+
+						"&id="+Math.random();
+		//Hacemos la peticion remota
+
+		//Validamos que no estén vacíos
+		if(usuario!="" && nombre!="" && clave!="" && tipo!="")
+		{
+			$.ajax({
+				cache:false,
+				type:"POST",
+				dataType:"json",
+				url:"php/utilerias.php",
+				data:parametros,
+				success:function(response){
+					//Si todo sale bien
+					if(response.respuesta == true)
+					{
+						alert("Nuevo usuario registrado exitosamente");
+					}
+					else
+					{
+						alert("Error al registrar usuario");
+					}
+				},
+				error:function(xhr, ajaxOptions, thrownError){
+					//Si todo sale mal
+				}
+			});
+		}
+		else
+		{
+			alert("Todos los campos son obligatorios");
+		}
+	}
+
+	var CambioUsuario = function()
+	{
+		//javascript se conecta a php y este devuelve un JSON
+		//Extraer los datos de los inputs en el HTML
+		var usuario= $("#txtUsuarioCambio").val();
+		var dato= $("#txtDatoCambio").val();
+		var cambio= $("#txtCambio").val();
+
+		//Preparar los parámetros para AJAX
+		var parametros="opcion=cambio"+
+						"&usuario="+usuario+
+						"&dato="+dato+
+						"&cambio="+cambio+
+						"&id="+Math.random();
+		//Hacemos la peticion remota
+
+		//Validamos que no estén vacíos
+		if(usuario!="" && dato!="" && cambio!="")
+		{
+			$.ajax({
+				cache:false,
+				type:"POST",
+				dataType:"json",
+				url:"php/utilerias.php",
+				data:parametros,
+				success:function(response){
+					//Si todo sale bien
+
+					if(response.respuesta == true)
+					{
+						alert("Usuario "+usuario+" modificado exitosamente");
+					}
+					else
+					{
+						alert("No existe el usuario "+usuario);
+
+					}
+				},
+				error:function(xhr, ajaxOptions, thrownError){
+					//Si todo sale mal
+				}
+			});
+		}
+		else
+		{
+			alert("Todos los campos son obligatorios");
+		}
+	}
+
+	var BajaUsuario = function()
+	{
+		//javascript se conecta a php y este devuelve un JSON
+		//Extraer los datos de los inputs en el HTML
+		var usuario= $("#txtUsuarioBaja").val();
+
+		//Preparar los parámetros para AJAX
+		var parametros="opcion=baja"+
+						"&usuario="+usuario+
+						"&id="+Math.random();
+		//Hacemos la peticion remota
+
+		//Validamos que no estén vacíos
+		var respuesta_ajax = $.ajax({
+			cache:false,
+			type:"POST",
+			dataType:"json",
+			url:"php/utilerias.php",
+			data:parametros,
+			success:function(response){
+				//Si todo sale bien
+				if(response.respuesta == true)
+				{
+					alert("Usuario "+usuario+" eliminado exitosamente");
+				}
+				else
+				{
+					alert("Usuario "+usuario+" no encontrado");
+				}
+			},
+			error:function(xhr, ajaxOptions, thrownError){
+				//Si todo sale mal
+				alert("error de algo al eliminar");
+			}
+		});
+	}
+
+	var ConsultaUsuario = function()
+	{
+		//javascript se conecta a php y este devuelve un JSON
+		//Extraer los datos de los inputs en el HTML
+		var usuario= $("#txtUsuarioConsulta").val();
+
+		//Preparar los parámetros para AJAX
+		var parametros="opcion=consulta"+
+						"&usuario="+usuario+
+						"&id="+Math.random();
+		//Hacemos la peticion remota
+
+		//Validamos que no estén vacíos
+		var respuesta_ajax = $.ajax({
+			cache:false,
+			type:"POST",
+			dataType:"json",
+			url:"php/utilerias.php",
+			data:parametros,
+			success:function(response){
+			//Use response
+				if(response.respuesta=="")
+				{
+					alert("No existe el usuario "+usuario);
+				}
+				else
+				{
+					$("#tablita").html(response.respuesta);
+
+				}
+			
+			},
+			error:function(mes){
+					//Si todo sale mal
+					//console.log(mes);
+			}
+		});
+	}
+	
 	var Alta = function()
 	{
 		$("#artAltaUsuario").show("slow");
 	}
-	var GuardaUsuario = function()
+	var Baja = function()
 	{
-
+		$("#artBajaUsuario").show("slow");
 	}
+	var Cambio = function()
+	{
+		$("#artCambioUsuario").show("slow");
+	}
+	var Consulta = function()
+	{
+		$("#artConsultaUsuario").show("slow");
+	}
+
 	//keypress se ejecuta cada vez que presiono una tecla sobre el input
 	$("#txtClave").on("keypress",teclaClave);
+
 	$("#btnAlta").on("click",Alta);
+	$("#btnConsulta").on("click",Consulta);
+	$("#btnCambio").on("click",Cambio);
+	$("#btnBaja").on("click",Baja);
+
+	$("#btnValidaUsuario").on("click",validaUsuario);
 	$("#btnGuardaUsuario").on("click",GuardaUsuario);
-
-
+	$("#btnBajaUsuario").on("click",BajaUsuario);
+	$("#btnCambioUsuario").on("click",CambioUsuario);
+	$("#btnConsultaUsuario").on("click",ConsultaUsuario);
+	
 }
 //Evento inicial
 $(document).on("ready",inicioUsuarios);
